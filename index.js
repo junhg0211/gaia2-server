e9000983157deacc7d91348e99e9a734019d8748
Sch <junhg0211@gmail.com>
2025-11-14 14:47:29 +0900

    websocket server

---
+++ index.js
@@ -0,0 +1,21 @@
+import { WebSocketServer } from 'ws';
+
+const port = 48829;
+const host = "0.0.0.0";
+
+const wss = new WebSocketServer({ port, host });
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
